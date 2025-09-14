dbbbd82a8e451a542fc05b4c4163b61befdbe058
Роман Зорин <sgena170315@gmail.com>
2025-09-14 14:39:59 +0300

    Первичный конструктор

--- src/Spell.kt
+++ src/Spell.kt
@@ -1,16 +1,9 @@
-class Spell {
-    val name: String
-    val width: Int
-    val height: Int
+class Spell(
+    val name: String,
+    val width: Int,
+    val height: Int,
     val symbol: String
-
-    constructor(name: String, width: Int, height: Int, symbol: String) {
-        this.name = name
-        this.width = width
-        this.height = height
-        this.symbol = symbol
-    }
-
+) {
     constructor(name: String, size: Int, symbol: String) : this(name, size, size, symbol)
 
     constructor() : this("", 0, 0, "")
