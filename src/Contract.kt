b3909066569cecbbe21c3ec2bdc95976ba611961
Роман Зорин <sgena170315@gmail.com>
2025-09-14 14:43:42 +0300

    Class Contract и его реализация

---
+++ src/Contract.kt
@@ -0,0 +1,13 @@
+class Contract(
+    val clientName: String,
+    val taskDescription: String,
+    val reward: Int,
+    val isUrgent: Boolean = false
+) {
+    fun printContractInfo() {
+        println("Заказчик: $clientName")
+        println("Задача: $taskDescription")
+        println("Награда: $reward")
+        println("Срочный: ${if (isUrgent) "Срочно!" else "Обычный контракт"}")
+    }
+}
---
+++ src/ContractR.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val contract1 = Contract("Чухан Элрик", "Охотиться на гоблинов в лесу", 500, true)
+    val contract2 = Contract("Мопс Альберт", "Доставить товар в соседний город", 300)
+
+    contract1.printContractInfo()
+    println()
+    contract2.printContractInfo()
+}
