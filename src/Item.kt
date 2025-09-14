b132266977bbd7c63bb0a8b1b9c3d275382ca160
Роман Зорин <sgena170315@gmail.com>
2025-09-14 15:05:00 +0300

    Задание 1: Создание класса Item (предмет) с несколькими конструкторами

--- src/Item.kt
+++ src/Item.kt
@@ -1,6 +1,19 @@
-class Item(val name: String, val price: Int) {
-    fun displayInfo() {
-        println("Товар: $name, Цена: $price")
+class Item {
+    val name: String
+    val type: String
+    val value: Int
+
+    constructor(name: String, type: String, value: Int) {
+        this.name = name
+        this.type = type
+        this.value = value
+    }
+
+    constructor(name: String, type: String) : this(name, type, 0)
+
+    fun printInfo() {
+        println("Предмет: $name")
+        println("Тип: $type")
+        println("Стоимость: $value монет")
     }
 }
-
